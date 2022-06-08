cf5a4f48303920e0535a016ef430b2a2ee53d557
Sxmon17 <simonguglberger@gmail.com>
2022-06-08 07:16:29 +0200

    optimizied code and fixed minor bugs

--- app/src/main/java/AssignMan/GraphMail.java
+++ app/src/main/java/AssignMan/GraphMail.java
@@ -1,26 +1,26 @@
 package AssignMan;
 
-import com.microsoft.graph.models.Event;
 import com.microsoft.graph.models.Message;
-import com.microsoft.graph.requests.EventCollectionRequestBuilder;
 import com.microsoft.graph.requests.GraphServiceClient;
 import com.microsoft.graph.requests.MessageCollectionPage;
+import okhttp3.Request;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import static AssignMan.GraphAuth.authProvider;
 
 public class GraphMail {
     public static void getMails(){
         System.out.println("test");
-        GraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider( authProvider ).buildClient();
-        MessageCollectionPage messages = graphClient.me().messages()
-                .buildRequest()
+        GraphServiceClient<Request> graphClient = GraphServiceClient.builder().authenticationProvider( authProvider ).buildClient();
+        MessageCollectionPage messages = Objects.requireNonNull(graphClient.me().messages()
+                        .buildRequest())
                 .select("sender,subject,bodyPreview")
                 .get();
-        List<Message> allMessages = new LinkedList<>();
-        allMessages.addAll(messages.getCurrentPage());
+        assert messages != null;
+        List<Message> allMessages = new LinkedList<>(messages.getCurrentPage());
         for (Message message : allMessages) {
             assert message.sender != null;
             System.out.println(message.bodyPreview);
